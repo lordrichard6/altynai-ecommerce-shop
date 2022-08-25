c6497edd56198e50b5d898fbd6eae2bb90183427
lordrichard6 <paulolopesreizinho@gmail.com>
2022-08-25 22:10:53 +0200

    task:create cart page

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -8,7 +8,7 @@ import { Store } from "../../utils/Store";
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store);
-
+    const router = useRouter()
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
@@ -25,7 +25,9 @@ export default function ProductScreen() {
         return
     }
 
-    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } 
+    });
+    router.push('/cart');
   };
 
   return (
