8e0fa5853fe9336c52518e2da8146f945df180a2
lordrichard6 <paulolopesreizinho@gmail.com>
2022-08-25 21:04:09 +0200

    task:add items to cart

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../public/altynai_logo.png";
+import { Store } from "../utils/Store";
 
 export default function Layout({ title, children }) {
+  const {state } = useContext(Store)
+  const {cart}= state
   return (
     <>
       <Head>
@@ -12,9 +15,9 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex min-h-screen flex-col justify-between">
+      <div className="flex min-h-screen flex-col justify-between page-background">
         <header>
-          <nav className="flex h-22 justify-between items-center shadow-md px-4">
+          <nav className="flex h-22 justify-between items-center shadow-md px-4 nav-background">
             <Link href="/">
               <a className="text-lg font-bold w-2">
                 <Image
@@ -28,7 +31,13 @@ export default function Layout({ title, children }) {
             </Link>
             <div>
               <Link href="/cart">
-                <a className="p-2">Cart</a>
+                <a className="p-2">Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
+                  </span>
+                )}
+                </a>
               </Link>
               <Link href="/login">
                 <a className="p-2">Login</a>
