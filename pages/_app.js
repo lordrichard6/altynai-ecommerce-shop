f2d4a46d87ea8ba022fe4541127c4987b1d005c6
lordrichard6 <paulolopesreizinho@gmail.com>
2022-08-30 21:47:12 +0200

    task:connect with paypal

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,29 @@ import '../styles/globals.css'
 import { SessionProvider, useSession } from 'next-auth/react'
 import { StoreProvider } from '../utils/Store'
 import { useRouter } from 'next/router'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        {Component.auth ? (
-          <Auth>
+        <PayPalScriptProvider session={session}>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        ) : (
-          <Component {...pageProps} />
-        )}
+          )}
+        </PayPalScriptProvider>
       </StoreProvider>
     </SessionProvider>
   )
 }
 
-function Auth({children}) {
+function Auth({ children }) {
   const router = useRouter()
-  const {status} = useSession({
+  const { status } = useSession({
     required: true,
     onUnauthenticated() {
       router.push('/unauthorized?message=login required')
