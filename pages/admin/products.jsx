613bc2ef955a54273d633f18fc686c3378cb29bf
lordrichard6 <paulolopesreizinho@gmail.com>
2022-09-06 13:03:01 +0200

    task: create/delete products

--- pages/admin/products.jsx
+++ pages/admin/products.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useEffect, useReducer } from "react";
 import Layout from "../../components/Layout";
+import { toast } from "react-toastify";
 import { getError } from "../../utils/error";
 
 function reducer(state, action) {
@@ -12,17 +14,51 @@ function reducer(state, action) {
       return { ...state, loading: false, products: action.payload, error: "" };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
+    case "CREATE_REQUEST":
+      return { ...state, loadingCreate: true };
+    case "CREATE_SUCCESS":
+      return { ...state, loadingCreate: false };
+    case "CREATE_FAIL":
+      return { ...state, loadingCreate: false };
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true };
+    case "DELETE_SUCCESS":
+      return { ...state, loadingDelete: false, successDelete: true };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false };
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       return state;
   }
 }
 
 export default function AdminProductScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+  const router = useRouter();
+  const [
+    { loading, error, products, loadingCreate, successDelete, loadingDelete },
+    dispatch,
+  ] = useReducer(reducer, {
     loading: true,
     products: [],
     error: "",
   });
+
+  const createHandler = async () => {
+    if (!window.confirm("Are you sure?")) {
+      return;
+    }
+    try {
+      dispatch({ type: "CREATE_REQUEST" });
+      const { data } = await axios.post(`/api/admin/products`);
+      dispatch({ type: "CREATE_SUCCESS" });
+      toast.success("Product created successfully");
+      router.push(`/admin/product/${data.product._id}`);
+    } catch (err) {
+      dispatch({ type: "CREATE_FAIL" });
+      toast.error(getError(err));
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,8 +71,28 @@ export default function AdminProductScreen() {
       }
     };
 
-    fetchData();
-  }, []);
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
+
+  const deleteHandler = async (productId) => {
+    if (!window.confirm("Are you sure?")) {
+      return;
+    }
+    try {
+      dispatch({ type: "DELETE_REQUEST" });
+      await axios.delete(`/api/admin/products/${productId}`);
+      dispatch({ type: "DELETE_SUCCESS" });
+      toast.success("Product deleted successfully");
+    } catch (err) {
+      dispatch({ type: "DELETE_FAIL" });
+      toast.error(getError(err));
+    }
+  };
+
   return (
     <Layout title="Admin Products">
       <div className="grid md:grid-cols-4 md:gap-5">
@@ -59,7 +115,17 @@ export default function AdminProductScreen() {
           </ul>
         </div>
         <div className="overflow-x-auto md:col-span-3">
-          <h1 className="mb-4 text-xl">Products</h1>
+          <div className="flex justify-between">
+            <h1 className="mb-4 text-xl">Products</h1>
+            {loadingDelete && <div>Deleting item...</div>}
+            <button
+              disabled={loadingCreate}
+              onClick={createHandler}
+              className="primary-button"
+            >
+              {loadingCreate ? "Loading" : "Create"}
+            </button>
+          </div>
           {loading ? (
             <div>Loading...</div>
           ) : error ? (
@@ -88,13 +154,17 @@ export default function AdminProductScreen() {
                       <td className=" p-5 ">{product.countInStock}</td>
                       <td className=" p-5 ">{product.rating}</td>
                       <td className=" p-5 ">
-                      <Link href={`/admin/product/${product._id}`}>
+                        <Link href={`/admin/product/${product._id}`}>
                           <a type="button" className="default-button">
                             Edit
                           </a>
                         </Link>
                         &nbsp;
-                        <button className="default-button" type="button">
+                        <button
+                          className="default-button"
+                          type="button"
+                          onClick={() => deleteHandler(product._id)}
+                        >
                           Delete
                         </button>
                       </td>
