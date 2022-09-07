5cbe48bf85b7164c0028b3bf5947342b4b71cb57
lordrichard6 <paulolopesreizinho@gmail.com>
2022-09-07 11:32:10 +0200

    chore:favicon icons added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -33,8 +33,13 @@ export default function Layout({ title, children }) {
       <Head>
         <title>{title ? title + " - Altynai Fashion" : "Altynai Fashion"}</title>
         <meta name="description" content="Ecommerce Website" />
-        <link rel="icon" type="image/x-icon" href="/icon.ico" />
-        <link rel="shortcut icon" href="/icon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <ToastContainer position="bottom-center" limit={1} />
       <div className="flex min-h-screen flex-col justify-between page-background">
